d0119c77cfea59b1b7e0b4293f8de5fa333c4cf1
joonwoo88 <jwkim@gncloud.kr>
2018-09-18 18:37:42 +0900

    데이터 생성 완료

--- data_zigzag_weight.py
+++ data_zigzag_weight.py
@@ -1,4 +1,5 @@
 import os
+import math
 
 import pandas
 import pandas as pd
@@ -32,31 +33,41 @@ for i in range(1, len(chart_data['action'])):
         start_index = 0
 
     if tmp_action != act:
-        print('range: {} ~ {}, count: {}, action: {} => {}'.format(start_index, i, (i - start_index), tmp_action, act))
         is_append = True
     elif i == len(chart_data['action']) - 1:
-        print('last > range: {} ~ {}, count: {}, action: {} => {}'.format(start_index, i, (i - start_index), tmp_action, act))
         is_append = True
 
     if is_append:
+        # print('last > range: {} ~ {}, count: {}, action: {}, next: {} => {}'.format(start_index, i, (i - start_index),
+        #                                                                             tmp_action, tmp_action, act))
         cnt = i - start_index
+        weight = 1.0
         for c in range(cnt):
             if c < offset:
                 w = 0.0
-            elif tmp_action == 'U':
-                w = 1 / (cnt - 1)
+            elif c == offset:
+                w = 1.0 if tmp_action == 'U' else -1.0
             else:
-                w = -(1 / (cnt - 1))
-            weight_list.append(float(w))
+                weight -= 1 / cnt
+                # 1.0, -1.0 벗어나면 소수점 제거..?
+                weight = weight * 1 / weight if weight > 1.0 or weight < -1.0 else weight
+
+                if tmp_action == 'U':
+                    w = weight
+                elif tmp_action == 'D':
+                    w = -weight
+
+            print('action: {}, count: {}/{} weight: {}'.format(tmp_action, c, cnt, w))
+            weight_list.append(w)
         tmp_action = act
         start_index = i
 
 weight_list = np.array(weight_list)
 # 0번 데이터는 이전 데이터가 없으므로 삭제.
-chart_data = chart_data.drop(0)
-chart_data = chart_data.drop(columns='action')
-chart_data['weight'] = np.array(weight_list)
-chart_data.to_csv(fpath + write_weight_file_name, mode='w', header=False)
+write_chart_data = chart_data.drop(0)
+write_chart_data = write_chart_data.drop(columns='action')
+write_chart_data['weight'] = np.array(weight_list)
+write_chart_data.to_csv(fpath + write_weight_file_name, mode='w', header=False)
 print('저장 완료.')
 
 
@@ -81,8 +92,10 @@ def ohlcv_polt(data):
 
 
 def plot_pivots(X, weight_list):
+    X = X.drop(0)
     X = np.array(X)
-    plt.plot(np.arange(len(X))[weight_list == 0.0], np.array(X)[weight_list == 0.0], 'ro')
+    plt.plot(np.arange(len(X))[weight_list >= 0.0], np.array(X)[weight_list >= 0.0], 'go')
+    plt.plot(np.arange(len(X))[weight_list <= 0.0], np.array(X)[weight_list <= 0.0], 'ro')
     # plt.plot(np.arange(len(X))[weight_list > 0.0], np.array(X)[weight_list > 0.0], color='b')
 
 
